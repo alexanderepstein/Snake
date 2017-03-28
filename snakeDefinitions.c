f2a22bdea3644102196aacf518dc1ca46fe04afc
David Wigley <dwigley92@gmail.com>
2017-03-28 11:00:25 -0400

    Adding Logic For Deleting Dynamic Memory
    - Fixing potential memory leaks
    - Changed naming convention for added clarity

--- snakeDefinitions.c
+++ snakeDefinitions.c
@@ -10,11 +10,11 @@ struct Node{
 };
 
 struct Snake{
-	struct Node *head;
+	struct Node *firstNode;
 };
 
 //setting up head here
-struct Snake *head;
+extern struct Snake *head;
 
 void initi(){
 	 head = malloc(sizeof(struct Snake));
@@ -27,24 +27,24 @@ void initi(){
 void insertLink(struct Snake *currentLink){
 	//INSERTING NEW GUY DIRECTLY AFTER HEAD. efficiency reasons
 	struct Node *newNode = malloc(sizeof(struct Node));
-	newNode->previous = currentLink->head; //new guy points back
-	newNode->next = currentLink->head->next; //new guy says the next thing is the link after the head
-	currentLink->head->next = newNode; //old guy says the next thing is the new guy
+	newNode->previous = currentLink->firstNode; //new guy points back
+	newNode->next = currentLink->firstNode->next; //new guy says the next thing is the link after the firstNode
+	currentLink->firstNode->next = newNode; //old guy says the next thing is the new guy
 	
-	newNode->xPosition = currentLink->head->xPosition;
-	newNode->yPosition = currentLink->head->yPosition;
+	newNode->xPosition = currentLink->firstNode->xPosition;
+	newNode->yPosition = currentLink->firstNode->yPosition;
 	
 	//can do preincrement / decrement for efficiency later on
 	if (direction == POS_X){
-		currentLink->head->xPosition = currentLink->head->xPosition + 1; 
+		currentLink->firstNode->xPosition = currentLink->firstNode->xPosition + 1; 
 	}else if (direction == NEG_X){
-		currentLink->head->xPosition = currentLink->head->xPosition - 1;
+		currentLink->firstNode->xPosition = currentLink->firstNode->xPosition - 1;
 	}else if (direction == POS_Y){
 		//NOTE A POSITIVE Y IS TECHNICALLY DOWN
-		currentLink->head->yPosition = currentLink->head->yPosition + 1;
+		currentLink->firstNode->yPosition = currentLink->firstNode->yPosition + 1;
 	}else if (direction == NEG_Y){
 		//NOTE A NEGATIVE Y IS TECHNICALLY UP
-		currentLink->head->yPosition = currentLink->head->yPosition - 1;
+		currentLink->firstNode->yPosition = currentLink->firstNode->yPosition - 1;
 	}
 }
 
@@ -54,29 +54,52 @@ void insertLink(struct Snake *currentLink){
  * link while the rest of the snake remains stationary.
  */
 void move(struct Snake *top){
-	struct Node *currentLink = top->head;
+	struct Node *currentLink = top->firstNode;
 	while (currentLink->next !=0){
 		currentLink = currentLink->next;
 	}
 	//at this point I have reached the end of the list
-	currentLink->xPosition = top->head->xPosition;
-	currentLink->yPosition = top->head->yPosition;
+	currentLink->xPosition = top->firstNode->xPosition;
+	currentLink->yPosition = top->firstNode->yPosition;
 	
 	currentLink->previous->next = 0; //set the new tail
-	currentLink->next = top->head->next; //set the 2nd position to point to the third
-	currentLink->previous = top->head; //set the 2nd position's previous to point back to head
-	top->head->next = currentLink; //set the head's next to point to the second position
+	currentLink->next = top->firstNode->next; //set the 2nd position to point to the third
+	currentLink->previous = top->firstNode; //set the 2nd position's previous to point back to firstNode
+	top->firstNode->next = currentLink; //set the firstNode's next to point to the second position
 	
 	//can do preincrement / decrement for efficiency later on
 	if (direction == POS_X){
-		top->head->xPosition = top->head->xPosition + 1; 
+		top->firstNode->xPosition = top->firstNode->xPosition + 1; 
 	}else if (direction == NEG_X){
-		top->head->xPosition = top->head->xPosition - 1;
+		top->firstNode->xPosition = top->firstNode->xPosition - 1;
 	}else if (direction == POS_Y){
 		//NOTE A POSITIVE Y IS TECHNICALLY DOWN
-		top->head->yPosition = top->head->yPosition + 1;
+		top->firstNode->yPosition = top->firstNode->yPosition + 1;
 	}else if (direction == NEG_Y){
 		//NOTE A NEGATIVE Y IS TECHNICALLY UP
-		top->head->yPosition = top->head->yPosition - 1;
+		top->firstNode->yPosition = top->firstNode->yPosition - 1;
 	}
 }
+
+/**
+ * Function for deleting all of the snake's dynamic memory.
+ * Prventing leaks.
+ */
+ void deleteSnake(struct Snake *top){
+	 struct Node *currentLink = top->firstNode;
+	 //get to the end of the list
+	 while (currentLink->next !=0){
+		currentLink = currentLink->next;
+	}
+	struct Node *temp; //temporay pointer that will store an additional link to prevent orphaning
+	
+	//check that there is something before it
+	while(currentLink->previous !=0){
+		temp = currentLink; //set temporary pointer
+		temp=currentLink->previous; //go back one
+		free currentLink; //delete the current one
+		currentLink = temp; //slide our tracking variable 
+	}
+	free currentLink; //last link
+	free top; //dump the pointer at the head of the snake
+ }
